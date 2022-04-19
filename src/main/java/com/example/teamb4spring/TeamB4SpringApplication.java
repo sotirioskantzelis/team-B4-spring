b4c266b12c8f4efbc6b75a90b2fa126368bc0587
someone <someone@someplace.com>
2022-04-19 07:54:26 +0200

    initializing project

---
+++ src/main/java/com/example/teamb4spring/TeamB4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamb4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamB4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamB4SpringApplication.class, args);
+    }
+
+}
