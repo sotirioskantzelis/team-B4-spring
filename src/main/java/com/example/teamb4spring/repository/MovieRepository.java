c02796181c31fef91b045c3eec0e47bf9deb1b82
sotirios.kantzelis <sotirios.kantzelis@eurodyn.com>
2022-04-20 14:06:58 +0300

    query Movie

--- src/main/java/com/example/teamb4spring/repository/MovieRepository.java
+++ src/main/java/com/example/teamb4spring/repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package com.example.teamb4spring.repository;
 
 import com.example.teamb4spring.domain.Movie;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie,Long> {
+public interface MovieRepository extends JpaRepository<Movie,Long>,
+    QuerydslPredicateExecutor<Movie> {
+
+  List<Movie> findMoviesByTitleOrDescriptionIgnoreCaseContains(String name);
 }
